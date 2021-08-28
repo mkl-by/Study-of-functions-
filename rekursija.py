8cb6e5a41651bbc141bca6bd6fd6265af5d85a10
mkl-by <mkl-by@yandex.ru>
2021-08-28 20:21:23 +0300

    rekursija2

--- rekursija.py
+++ rekursija.py
@@ -23,6 +23,38 @@ def list_sum(L: list) -> int:
                 tot += list_sum(i)
         return tot
 
+
+''' факториал
+    5!=1*2*3*4*5
+    4!=1*2*3*4
+    3!=1*2*3
+    2!=1*2
+    1!=1
+    0!=1
+    '''
+def factorial(N: int) -> int:
+    """
+    factorial(5) -> 120
+    factorial(4) -> 24
+    factorial(3) -> 6
+    factorial(2) -> 2
+    factorial(1) -> 1
+    factorial(0) -> 1
+    """
+    return N*factorial(N-1) if N else 1
+
+"""
+    hello
+    hell
+    hel
+    he
+    h
+"""
+def pars_str(stroka: str) -> None:
+    print(stroka)
+    return pars_str(stroka[:-1]) if stroka else 0
+
+
 if __name__ == '__main__':
     L = [1, 2, 3]
     L1 = [1, [2, [3, 4], 5], 6, [7, 8]]
@@ -30,7 +62,13 @@ if __name__ == '__main__':
     print(ssum(L))
 
     print(list_sum(L1))
-    print(list_sum ([1, [2, [3, [4, [5]]]]]))
-    print(list_sum([[[[ [1], 2], 3], 4], 5]))
-# Выводит 15 (перегруженное справа)
-# Выводит 15 (п
+    print(list_sum([1, [2, [3, [4, [5]]]]]))
+    print(list_sum([[[[[1], 2], 3], 4], 5]))
+    # Выводит 15 (перегруженное справа)
+    # Выводит 15 (перегруженное слева)
+    print('*****Факториал******')
+    print(f'факториал 5={factorial(5)}')
+
+    print('*****Cтрока******')
+    pars_str('hello')
+
