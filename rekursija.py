24ea231c5bef5592334ca7719c1a939126ec50c6
mkl-by <mkl-by@yandex.ru>
2021-08-29 13:43:20 +0300

    rekursija1

--- rekursija.py
+++ rekursija.py
@@ -4,6 +4,7 @@ def mysum(L):
     if not L: return 0
     return nonempty(L)
 
+
 def nonempty(L):
     return L[0]+mysum(L[1:])
 
@@ -32,6 +33,8 @@ def list_sum(L: list) -> int:
     1!=1
     0!=1
     '''
+
+
 def factorial(N: int) -> int:
     """
     factorial(5) -> 120
@@ -43,16 +46,87 @@ def factorial(N: int) -> int:
     """
     return N*factorial(N-1) if N else 1
 
-"""
-    hello
-    hell
-    hel
-    he
-    h
-"""
+
 def pars_str(stroka: str) -> None:
+    """
+     hello
+     hell
+     hel
+     he
+     h
+ """
     print(stroka)
     return pars_str(stroka[:-1]) if stroka else 0
+
+
+def pars_str1(stroka: str) -> str:
+    """
+     hello
+     hell
+     hel
+     he
+     h
+    ниже вызовы стека для закоментированной строки иначе елка перевернется
+     pars_str1(hello) - '\nh\nhe\nhel\nhell\nhello'
+     pars_str1(hell)  - '\nh\nhe\nhel\nhell'
+     pars_str1(hel)   - '\nh\nhe\nhel'
+     pars_str1(he)    - '\nh\nhe'
+     pars_str1(h)     - '\nh'
+     pars_str1()      - ''
+    для незакоментированной строки елка развернулась
+     pars_str1(hello) - 'hello\nhell\nhel\nhe\nh\n'
+     pars_str1(hell)  - 'hell\nhel\nhe\nh\n'
+     pars_str1(hel)   - 'hel\nhe\nh\n'
+     pars_str1(he)    - 'he\nh\n'
+     pars_str1(h)     - 'h\n'
+     pars_str1()      - ''
+
+ """
+    if stroka:
+        return f'{stroka}\n{pars_str1(stroka[:-1])}'
+        # return f'{pars_str1(stroka[:-1])}\n{stroka}''
+    return ''
+
+
+def print_star(N: int) -> None:
+    print("*"*N)
+    if N:
+        print_star(N-1)
+    print("*"*N)
+    return 0
+
+
+def print_stars(N: int) -> str:
+    """
+    print_stars(5) - *\n*\n*\n*\n*\n
+    print_stars(4) - *\n*\n*\n*\n
+    print_stars(3) - *\n*\n*\n
+    print_stars(2) - *\n*\n
+    print_stars(1) - *\n
+    print_stars(0) - ''
+    """
+    # if N:
+    #     return f'*\n{print_stars(N-1)}'
+    # return ''
+    return '' if not N else f'*\n{print_stars(N-1)}'
+
+
+def febonachi(N: int) -> int:
+    """
+        0,1,1,2,3,5,8,13,21,...., где N количество чисел Фeбоначи которых нужно найти
+        febonachi(5) - [0,1,1,2,3,5]
+        febonachi(4) - [0,1,1,2,3]
+        febonachi(3) - [0,1,1,2]
+        febonachi(2) - [0,1,1]
+        febonachi(1) - [0, 1]
+    """
+    if N == 0:
+        return [0]
+    if N == 1:
+        return [0, 1]
+    res = febonachi(N-1)  # при N=2 -> [0,1]
+    res.append(res[-1]+res[-2])
+    return res
 
 
 if __name__ == '__main__':
@@ -66,9 +140,21 @@ if __name__ == '__main__':
     print(list_sum([[[[[1], 2], 3], 4], 5]))
     # Выводит 15 (перегруженное справа)
     # Выводит 15 (перегруженное слева)
-    print('*****Факториал******')
+    print('***** Факториал ******')
     print(f'факториал 5={factorial(5)}')
 
-    print('*****Cтрока******')
+    print('***** Cтрока ******')
+    # кто последний попал в рекурсию, тот первый из нее выйдет
     pars_str('hello')
 
+    print('***** Cтрока1 ******')
+    print(pars_str1('hello'))
+
+    print('***** Звезда *****')
+    print_star(5)
+
+    print('***** Звезды *****')
+    print(print_stars(5))
+
+    print('***** Фeбоначи *****')
+    print(febonachi(25))
